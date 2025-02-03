69a02669ce71cb4e662d176d529916534dd5c410
Pedro Luiz <pedrin.luiz22@gmail.com>
2025-02-03 12:09:54 -0300

    Main Menu WORKING
    Menu principal funcinando ja com troca de cena e menu de opçoes

---
+++ Assets/Pedrin/MainMenuManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField] private string sceneName;
+    [SerializeField] private GameObject optionsPanel;
+    [SerializeField] private GameObject mainMenuPanel;
+
+    public void Play()
+    {
+        SceneManager.LoadScene(sceneName);   
+    }
+
+    public void OpenOptions()
+    {
+        optionsPanel.SetActive(true);
+        mainMenuPanel.SetActive(false);
+    }
+
+    public void CloseOptions()
+    {
+        optionsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quiting...");
+        Application.Quit();
+    }
+}
