6ddce0338c382b3999ecd35a9baf77f6913e1e4e
Pedro de Paula <pedrodepaula2005@gmail.com>
2025-02-04 10:45:06 -0300

    Update FollowPlayerComponent.cs

--- Assets/Enemies/Scripts/FollowPlayerComponent.cs
+++ Assets/Enemies/Scripts/FollowPlayerComponent.cs
@@ -2,19 +2,42 @@ using UnityEngine;
 
 public class FollowPlayerComponent : MonoBehaviour
 {
+    [SerializeField] private float _rotateSpeed;
     [SerializeField] private float _followSpeed;
-    [SerializeField] private float _followDistance;
+    [SerializeField] private float _followRadius;
+
+    private Transform _player;
+    private Rigidbody2D _rb;
 
     private void Start()
     {
-        // TO DO: Get reference to player
+        _player = FindFirstObjectByType<PlayerMovement>().transform;
+        _rb = GetComponent<Rigidbody2D>();
     }
 
 
     private void Update()
     {
-        // TO DO: Compare distance with player to see if is in follow distance
-        
-        // TO DO: Use rb.velocity to follow player.
+        if (Vector2.Distance(_player.position, transform.position) < _followRadius)
+        {
+            Follow();
+        }
+    }
+
+    private void Follow()
+    {
+        Vector2 direction = (_player.position - transform.position).normalized;
+
+        Quaternion targetRotation = Quaternion.LookRotation(transform.up, direction);
+        Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotateSpeed * Time.deltaTime);
+        _rb.SetRotation(rotation);
+
+        _rb.linearVelocity = transform.forward * _followSpeed;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, _followRadius);
     }
 }
