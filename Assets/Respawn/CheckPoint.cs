84406de5e25f70af3c8b3e0b80ee528f6a219867
Pedro de Paula <pedrodepaula2005@gmail.com>
2025-02-05 17:04:42 -0300

    Respawnable Start

---
+++ Assets/Respawn/CheckPoint.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+[RequireComponent (typeof(BoxCollider2D))]
+public class CheckPoint : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Se o objeto de colisão tiver o componente IRespawnable, entra no if
+        if (collision.gameObject.TryGetComponent<IRespawnable>(out var respawnable))
+        {
+            // TO DO: Rodar animação
+
+            // Salva o novo spawnpoint e desliga esse código para não rodar nada aqui novamente
+            respawnable.SetSpawnPoint(transform);
+            Destroy(this);
+        }
+    }
+}
